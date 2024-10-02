396ac361c0c42e2ad99ec13f0a5a796e0ea1fff3
river <river@riverdeMacBook-Air.local>
2024-10-03 00:27:15 +0800

    first commit

---
+++ demo1/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world1111")
+	var a = "aaa"
+	fmt.Printf(a)
+	fmt.Println(a)
+	fmt.Printf("%v", a)
+
+	fmt.Printf("%v", "%v", "%v", a, a, a)
+
+}
